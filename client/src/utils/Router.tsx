a32c2fbdce513c4eea8da6a6d3642724bbf00176
KaganHamzacebi <kagan.hcebi@gmail.com>
2023-02-12 02:34:32 +0300

    Create page initialized

--- client/src/utils/Router.tsx
+++ client/src/utils/Router.tsx
@@ -1,18 +1,23 @@
 import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import {Home} from "../views/Home";
+import Create from "../views/Create";
 
 function Router() {
 
     const router = createBrowserRouter([
         {
             path: '/',
-            element: <Home />
+            element: <Home/>
+        },
+        {
+            path: '/create',
+            element: <Create/>
         }
     ])
 
     return (
         <>
-            <RouterProvider router={router} />
+            <RouterProvider router={router}/>
         </>
     )
 }
--- client/src/views/Create.tsx
+++ client/src/views/Create.tsx
@@ -1,8 +1,27 @@
 import styles from '../styles/Create.module.scss';
+import {useForm} from "react-hook-form";
+import Header from "../components/Header";
 
 function Create() {
+
+    type Item = {
+        title: string;
+        description: string;
+        tag: Array<string>;
+        images: Array<string>;
+        location: string;
+        created: string;
+    }
+
+    const {register, handleSubmit, watch, formState: {errors}} = useForm();
+    const onSubmit = (data: Item) => console.log(data);
+
     return (
         <>
+            <Header />
+            <div className={styles.main}>
+
+            </div>
         </>
     )
 }
